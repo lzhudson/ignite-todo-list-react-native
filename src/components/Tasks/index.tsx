b1b0b05dd2aedbc7e414ad63563f24733ab468ef
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 20:10:03 -0300

    feat: pass tasks state and setTasks dispatch for TaskList component

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -3,18 +3,20 @@ import { style } from "./styles";
 import { TaskHeader } from "../TaskHeader";
 import { TaskList } from "../TaskList";
 import { Task } from "../../screens/Home";
+import { Dispatch, SetStateAction } from "react";
 
 type Props = {
   tasks: Task[]
+  onUpdateTask: Dispatch<SetStateAction<Task[]>>
 }
 
-export function Tasks({ tasks } : Props) {
+export function Tasks({ tasks, onUpdateTask } : Props) {
   const quantityTasksFinished = tasks.filter(task => task.hasFinishedTask).length;
 
   return (
     <View style={style.container}>
       <TaskHeader quantityTasksCreated={tasks.length} quantityTasksFinished={quantityTasksFinished} />
-      <TaskList tasks={tasks} />
+      <TaskList onUpdateTask={onUpdateTask} tasks={tasks} />
     </View>
   )
 }
