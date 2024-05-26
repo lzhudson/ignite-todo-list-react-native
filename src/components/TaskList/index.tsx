0e15dd282aa92c2006a292ad5235638387be433f
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 20:14:35 -0300

    feat: pass id and onUpdateTask function

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -3,12 +3,14 @@ import { styles } from "./styles";
 import { ClipboardText } from "phosphor-react-native";
 import { TaskItem } from "../TaskItem";
 import { Task } from "../../screens/Home";
+import { Dispatch, SetStateAction } from "react";
 
 type Props = {
   tasks: Task[]
+  onUpdateTask: Dispatch<SetStateAction<Task[]>>
 }
 
-export function TaskList({ tasks } : Props) {
+export function TaskList({ tasks, onUpdateTask} : Props) {
   return (
     <View style={styles.container}>
       <FlatList 
@@ -16,8 +18,10 @@ export function TaskList({ tasks } : Props) {
         keyExtractor={task => String(task.id)}
         renderItem={({ item }) => (
           <TaskItem  
+            id={item.id}
             name={item.title}
             hasFinishedTask={item.hasFinishedTask}
+            onUpdateTask={onUpdateTask}
           />
         )}
       />
