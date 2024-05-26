a30bb2d1ac22b291950e3974acaf8bbe563da549
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 16:02:59 -0300

    feat: create function to add new task

--- src/components/AddTaskForm/index.tsx
+++ src/components/AddTaskForm/index.tsx
@@ -1,16 +1,37 @@
 import { TextInput, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
 import { PlusCircle } from "phosphor-react-native";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Task } from "../../screens/Home";
 
-export function AddTaskForm() {
+type Props = {
+  onAddTask: Dispatch<SetStateAction<Task[]>>
+}
+
+export function AddTaskForm({ onAddTask } : Props) {
+  const [taskTitle, setTaskTitle] = useState('');
+
+  function handleAddTask() {
+    const task = {
+      id: Math.random(),
+      title: taskTitle,
+      hasFinishedTask: false
+    };
+  
+    onAddTask(prevState => [...prevState, task]);
+    setTaskTitle('');
+  }
+
   return (
     <View style={style.container}>
       <TextInput 
         style={style.inputAddTask} 
         placeholder="Adicione uma nova tarefa" 
         placeholderTextColor="#808080"
+        value={taskTitle}
+        onChangeText={(value) => setTaskTitle(value)}
       />
-      <TouchableOpacity style={style.buttonAddTask}>
+      <TouchableOpacity onPress={handleAddTask} style={style.buttonAddTask}>
         <PlusCircle color="#F2F2F2" size={18} />
       </TouchableOpacity>
     </View>
