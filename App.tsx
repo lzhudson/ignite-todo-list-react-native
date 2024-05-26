fe992ac485446c2f5a0a14482c150bfbdfb666f1
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 20:22:10 -0300

    feat: load font

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import { Home } from "./src/screens/Home";
+import {  useFonts, Inter_700Bold, Inter_400Regular } from '@expo-google-fonts/inter';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <Home />
