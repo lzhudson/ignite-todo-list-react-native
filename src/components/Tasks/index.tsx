6abad91d9d24d15fa388b153db64eb4b376e9a02
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 16:01:11 -0300

    feat: pass tasks for TaskHeader and TaskList

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -2,12 +2,19 @@ import { Text, View } from "react-native";
 import { style } from "./styles";
 import { TaskHeader } from "../TaskHeader";
 import { TaskList } from "../TaskList";
+import { Task } from "../../screens/Home";
 
-export function Tasks() {
+type Props = {
+  tasks: Task[]
+}
+
+export function Tasks({ tasks } : Props) {
+  const quantityTasksFinished = tasks.filter(task => task.hasFinishedTask).length;
+
   return (
     <View style={style.container}>
-      <TaskHeader />
-      <TaskList />
+      <TaskHeader quantityTasksCreated={tasks.length} quantityTasksFinished={quantityTasksFinished} />
+      <TaskList tasks={tasks} />
     </View>
   )
 }
