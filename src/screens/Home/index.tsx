1aa7e83184bffb161d5edcd8ef376caa0b761b1d
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 16:00:55 -0300

    feat: create interface Task and pass main state tasks for components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,13 +3,22 @@ import { AddTaskForm } from "../../components/AddTaskForm";
 import { Header } from "../../components/Header";
 import { Tasks } from "../../components/Tasks";
 import { style } from "./styles";
+import { useState } from "react";
+
+export interface Task {
+  id: number;
+  title: string;
+  hasFinishedTask: boolean
+}
 
 export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   return (
     <View style={style.container}>
       <Header />
-      <AddTaskForm />
-      <Tasks />
+      <AddTaskForm onAddTask={setTasks} />
+      <Tasks tasks={tasks} />
     </View>
   )
 }
