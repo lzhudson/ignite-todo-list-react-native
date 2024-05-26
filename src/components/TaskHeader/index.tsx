c802c4bca5a1e588ab823f2df99f2b9ccd2adff3
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 16:02:21 -0300

    feat: render quantityTasksCreated and quantityTasksFinished property

--- src/components/TaskHeader/index.tsx
+++ src/components/TaskHeader/index.tsx
@@ -1,19 +1,24 @@
 import { Text, View } from "react-native";
 import { style } from "./styles";
 
-export function TaskHeader() {
+type Props = {
+  quantityTasksCreated: number;
+  quantityTasksFinished: number
+}
+
+export function TaskHeader({ quantityTasksCreated, quantityTasksFinished } : Props) {
   return (
     <View style={style.container}>
       <View style={style.containerItem}>
         <Text style={style.itemLabel}>Criadas</Text>
         <View style={style.itemQuantityContainer}>
-          <Text style={style.itemQuantity}>0</Text>
+          <Text style={style.itemQuantity}>{quantityTasksCreated}</Text>
         </View>
       </View>
       <View style={style.containerItem}>
         <Text style={{...style.itemLabel, ...style.itemLabelPurple}}>Concluídas</Text>
         <View style={style.itemQuantityContainer}>
-          <Text style={style.itemQuantity}>0</Text>
+          <Text style={style.itemQuantity}>{quantityTasksFinished}</Text>
         </View>
       </View>
     </View>
